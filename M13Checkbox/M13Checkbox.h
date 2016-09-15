964ccb1e8a833500a1e3732021d5820585089d51
Rogerio de Paula Assis <rogerio@freshmob.com.au>
2016-09-15 11:26:57 +1000

    v1.2 framework

---
+++ M13Checkbox/M13Checkbox.h
@@ -0,0 +1,20 @@
+//
+//  M13Checkbox.h
+//  M13Checkbox
+//
+//  Created by Rogerio de Paula Assis on 15/09/2016.
+//  Copyright © 2016 Brandon McQuilkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for M13Checkbox.
+FOUNDATION_EXPORT double M13CheckboxVersionNumber;
+
+//! Project version string for M13Checkbox.
+FOUNDATION_EXPORT const unsigned char M13CheckboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M13Checkbox/PublicHeader.h>
+
+#import <M13Checkbox/M13CheckboxControl.h>
+
